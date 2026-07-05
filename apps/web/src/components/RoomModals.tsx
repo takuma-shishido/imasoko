cf862e110bf1ed0dc992862f7b4128f6e4817f51
takuma-shishido <130330524+takuma-shishido@users.noreply.github.com>
2026-07-05 17:00:54 +0900

    chore(web): 接続失敗UI(ConnFailModal)を削除し RoomModals の配線を外す

--- apps/web/src/components/RoomModals.tsx
+++ apps/web/src/components/RoomModals.tsx
@@ -3,7 +3,6 @@ import { PinModal } from "./modals/PinModal";
 import { PermModal } from "./modals/PermModal";
 import { WarnPublicModal } from "./modals/WarnPublicModal";
 import { LeaveModal } from "./modals/LeaveModal";
-import { ConnFailModal } from "./modals/ConnFailModal";
 
 // 全モーダルのまとめ描画(各コンポーネントが自身のフラグで表示制御)。
 export function RoomModals() {
@@ -14,7 +13,6 @@ export function RoomModals() {
       <PermModal />
       <WarnPublicModal />
       <LeaveModal />
-      <ConnFailModal />
     </>
   );
 }
